f58764eb2d084cd16017533d2efc009b17268dd3
Montblanc <guillaume.zaugg@gmail.com>
2018-12-25 13:16:18 +0100

    modif dans exo_05.rb

--- exo_05.rb
+++ exo_05.rb
@@ -1,41 +1,41 @@
-# Affiche "On va compter le nombre d'heures de travail à THP"
+# Affiche le string "On va compter le nombre d'heures de travail à THP"
 puts "On va compter le nombre d'heures de travail à THP"
 
-# Affiche le string "Travail :", calcul 10 * 5 * 11 et affiche le résultat
+# Affiche le string "Travail :",  calcul 10 * 5 * 11 et affiche le résultat 550
 puts "Travail : #{10 * 5 * 11}"
 
-# Affiche le string "En minutes ça fait :", calcul 10 * 5 * 11 * 60 et affiche le résultat
+# Affiche le string "En minutes ça fait :", calcul 10 * 5 * 11 * 60 et affiche le résultat 33 000
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
-
+# Affiche le string "Et en secondes ?"
 puts "Et en secondes ?"
 
-
+# Calcul 10 * 5 * 11 * 60 * 60 et affiche le résultat 1 980 000
 puts 10 * 5 * 11 * 60 * 60
 
-
+# Affiche le string "Est-ce que c'est vrai que 3 + 2 < 5 - 7"
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7"
 
-
+# Calcul 3 + 2 et 5 - 7, compare le résultat et affiche la résultat false
 puts 3 + 2 < 5 - 7
 
-
+# Affiche le string "Ça fait combien 3 + 2 ?", calcul 3 + 2 et affiche le résutat 5
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 
-
+# Affiche le string "Ça fait combien 5 - 7 ?", calcul 5 -7 et affiche le résultat -2
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 
-
+# Affiche le string "Ok, c'est faux alors !"
 puts "Ok, c'est faux alors !"
 
-
+# Affiche le string "C'est drôle ça faisons-en plus :"
 puts "C'est drôle ça faisons-en plus :"
 
-
+# Affiche le string "Est-ce que 5 est plus grand que -2 ?", compare 5 et -2 et affiche le résultat true
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 
-
+# Affiche le string "Est-ce que 5 est supérieur ou égal à -2 ?", compare 5 et -2 et affiche le résultat true
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 
-
+# Affiche le string "Est-ce que 5 est inférieur ou égal à -2 ?", compare 5 et -2 et affiche le résultat false
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
